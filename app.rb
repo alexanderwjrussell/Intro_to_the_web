d2ce37833f10c3bfe0b8b9a261a5b0f80137763e
alexanderwjrussell <awjrussell@live.co.uk>
2016-10-10 17:06:56 +0100

    Chapter 14 complete. POST and GET requests sorted.

--- app.rb
+++ app.rb
@@ -8,8 +8,21 @@ get '/secret' do
   'This is a secret page'
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
+
+get '/cat-form' do
+  erb(:cat_form)
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:named)
+end
+
+
 
 set   :session_secret, "here be dragons"
